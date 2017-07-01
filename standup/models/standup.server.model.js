8541ce1aad1e5d9f819fb7a46ce59c0c762d4633
Fitssum Haile <fitsum.wolde@gmail.com>
2017-07-01 14:37:11 -0400

    installed mongoose

---
+++ standup/models/standup.server.model.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var standupSchema = new Schema({
+  memberName: String,
+  project: String,
+  workYesterday: String,
+  workToday: String,
+  impediment: String,
+  createdOn: { type: Date, default: Date.now }
+});
+
+//disable (if we don't want id for some reason but usually mongoose creates one for us)
+var noIdSchema = new Schema({ name: String}, {_id: false});
+
+//example of using schema.add...you can also add multiple schema the same way..
+var exampleSchema = new Schema;
+exampleSchema.add({memberName: String});
