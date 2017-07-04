b1d4c39e1ee2c84af75c3fe2efa6526caf6edad3
Fitssum Haile <fitsum.wolde@gmail.com>
2017-07-04 11:19:59 -0400

    installed mongoose

--- standup/models/standup.server.model.js
+++ standup/models/standup.server.model.js
@@ -10,9 +10,16 @@ var standupSchema = new Schema({
   createdOn: { type: Date, default: Date.now }
 });
 
+//expose (export) a model...
+module.exports = mongoose.model('Standup', standupSchema);
+
+
+
+
+
 //disable (if we don't want id for some reason but usually mongoose creates one for us)
-var noIdSchema = new Schema({ name: String}, {_id: false});
+// var noIdSchema = new Schema({ name: String}, {_id: false});
 
 //example of using schema.add...you can also add multiple schema the same way..
-var exampleSchema = new Schema;
-exampleSchema.add({memberName: String});
+// var exampleSchema = new Schema;
+// exampleSchema.add({memberName: String});
--- standup/routes/index.js
+++ standup/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var stundupCtrl = require('../controllers/standup.server.controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/*Get new Note page*/
+router.get('/newnote', function(req, res) {
+  return stundupCtrl.getNote(req, res);
+});
+
+/* POST New Note page. */
+router.post('/newnote', function(req, res){
+  return stundupCtrl.create(req, res);
 });
 
 module.exports = router;
